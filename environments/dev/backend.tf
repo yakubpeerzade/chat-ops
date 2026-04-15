79f1d03d01de9e922e2778bbb3727b51b9e40657
yakubpeerzade <yakubpeerzade123@gmail.com>
2026-04-15 17:50:20 +0530

    created tf structure

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "chat-ops-tfstate-s3"
-    key            = "dev/${var.request_id}/terraform.tfstate"
+    key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "chat-ops-tf-lock"
   }
