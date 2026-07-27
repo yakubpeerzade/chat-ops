9a24484a5ede5894177799cba2b2765047b71c0e
yakubpeerzade <yakubpeerzade123@gmail.com>
2026-07-27 22:23:55 +0530

    deleted resources

--- environments/dev/ec2/main.tf
+++ environments/dev/ec2/main.tf
@@ -6,4 +6,7 @@ module "ec2" {
   instance_type      = var.instance_type
   subnet_id          = var.subnet_id
   security_group_ids = var.security_group_ids
+  instance_name      = var.instance_name
+  project_code       = var.project_code
+  project_owner      = var.project_owner
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -30,3 +30,52 @@ variable "root_volume_size"{
   default = 20 
 }
 variable "request_id" { type = string }
+
+variable "request_id" {
+  type        = string
+  description = "Unique ticket request ID"
+  default     = null
+}
+
+variable "os_name" {
+  type        = string
+  description = "Operating system (ubuntu, windows, amazonlinux23)"
+  default     = "ubuntu"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance size"
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Target Subnet ID"
+  default     = null
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  description = "List of Security Group IDs"
+  default     = []
+}
+
+# Added missing variable declarations referenced by main.tf
+variable "instance_name" {
+  type        = string
+  description = "Name tag for the EC2 instance"
+  default     = null
+}
+
+variable "project_code" {
+  type        = string
+  description = "Project identifier tag"
+  default     = null
+}
+
+variable "project_owner" {
+  type        = string
+  description = "Project requester/owner tag"
+  default     = null
+}
