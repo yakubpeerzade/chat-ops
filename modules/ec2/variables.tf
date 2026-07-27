45d95ea33420d321d5a93c8112a233b19072b5dc
yakubpeerzade <yakubpeerzade123@gmail.com>
2026-07-27 22:26:25 +0530

    deleted resources

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,36 +1,3 @@
-variable "os_name" {
-  type = string
-  validation {
-    condition     = contains(["ubuntu", "windows", "amazonlinux23"], var.os_name)
-    error_message = "os_name must be ubuntu, windows or amazonlinux23."
-  }
-}
-
-variable "instance_type" {
-  type = string
-  validation {
-    condition = contains([
-      "t2.micro", "t2.small", "t2.medium",
-      "t3.micro", "t3.small", "t3.medium", "t3.large",
-      "t3a.micro", "t3a.small", "t3a.medium",
-      "m5.large", "m5.xlarge",
-    ], var.instance_type)
-    error_message = "instance_type is not on the approved list."
-  }
-}
-
-variable "subnet_id"          { type = string }
-variable "security_group_ids" { type = list(string)}
-variable "key_name"{ 
-  type = string
-  default = null 
-}
-variable "root_volume_size"{ 
-  type = number
-  default = 20 
-}
-variable "request_id" { type = string }
-
 variable "request_id" {
   type        = string
   description = "Unique ticket request ID"
@@ -61,7 +28,6 @@ variable "security_group_ids" {
   default     = []
 }
 
-# Added missing variable declarations referenced by main.tf
 variable "instance_name" {
   type        = string
   description = "Name tag for the EC2 instance"
@@ -78,4 +44,10 @@ variable "project_owner" {
   type        = string
   description = "Project requester/owner tag"
   default     = null
+}
+
+variable "root_volume_size" {
+  type        = number
+  description = "Root volume size in GB"
+  default     = 20
 }
