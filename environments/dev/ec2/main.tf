67f97751a0dcb43c1eecf1d087847bbcff22927e
yakubpeerzade <yakubpeerzade123@gmail.com>
2026-07-27 21:08:09 +0530

    deleted resources

--- environments/dev/ec2/main.tf
+++ environments/dev/ec2/main.tf
@@ -1,9 +1,9 @@
 module "ec2" {
   source = "../../../modules/ec2"
 
-  ami_id         = var.ami_id
-  instance_type  = var.instance_type
-  instance_name  = var.instance_name
-  project_code   = var.project_code
-  project_owner  = var.project_owner
+  request_id         = var.request_id
+  os_name            = var.os_name
+  instance_type      = var.instance_type
+  subnet_id          = var.subnet_id
+  security_group_ids = var.security_group_ids
 }
--- environments/dev/ec2/variables.tf
+++ environments/dev/ec2/variables.tf
@@ -1,46 +1,52 @@
-# toggles
-variable "create_s3" {
-  type    = bool
-  default = false
+# Variables passed via .tfvars
+variable "request_id" {
+  type        = string
+  description = "Unique ticket request ID"
+  default     = null
 }
 
-variable "create_ec2" {
-  type    = bool
-  default = false
-}
-
-# S3
-variable "bucket_name" {
-  type    = string
-  default = null
-}
-
-# EC2
-variable "ami_id" {
-  type    = string
-  default = null
+variable "os_name" {
+  type        = string
+  description = "Operating system (ubuntu, windows, amazonlinux23)"
+  default     = "ubuntu"
 }
 
 variable "instance_type" {
-  type    = string
-  default = null
+  type        = string
+  description = "EC2 instance size"
+  default     = "t3.micro"
+}
+
+variable "root_volume_size" {
+  type        = number
+  description = "Root disk volume size in GB"
+  default     = 20
 }
 
 variable "instance_name" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
 }
 
-# common tags
 variable "project_code" {
-  type = string
+  type        = string
+  default     = null
 }
 
 variable "project_owner" {
-  type = string
+  type        = string
+  default     = null
 }
 
-# backend control
-variable "request_id" {
-  type = string
+# Optional network variables (defaulted to null so execution doesn't block)
+variable "subnet_id" {
+  type        = string
+  description = "Target Subnet ID"
+  default     = null
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  description = "List of Security Group IDs"
+  default     = []
 }
